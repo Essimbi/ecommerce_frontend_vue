957ddc37ecca6338712d2d2f1c683ad046c39988
Essimbi <essimbideranot@gmail.com>
2023-09-15 07:41:24 +0100

    Finition du composant ProductsListItem

---
+++ src/components/ProductsListItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['product'])
+</script>
+
+<template>
+    <div class="product-container">
+        <img :src="props.product.imageUrl" :alt="props.product.name" class="product-image">
+        <div class="details-wrap">
+            <h3>{{ props.product.name }}</h3>
+            <p>${{ props.product.price }}</p>
+        </div>
+        <button class="remove-button">Remove from cart</button>
+    </div>
+</template>
+
+<style scoped>
+.product-container {
+    align-content: 'center';
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 16px;
+    width: 100%;
+}
+
+.product-image {
+    flex: 1;
+    height: 100px;
+    max-width: 100px;
+}
+
+.details-wrap {
+    padding: 0 16px;
+    flex: 3;
+}
+
+.remove-button {
+    flex: 1;
+    margin: auto;
+}
+</style>
--- src/views/CardPage.vue
+++ src/views/CardPage.vue
@@ -14,14 +14,7 @@ const totalPrice = computed(() => {
 <template>
     <div id="page-wrap">
       <h1>Shoping cart</h1>
-      <div v-for="(product, index) in cartItems" :key="index" class="product-container">
-        <img :src="product.imageUrl" :alt="product.name" class="product-image">
-        <div class="details-wrap">
-          <h3>{{ product.name }}</h3>
-          <p>${{ product.price }}</p>
-        </div>
-        <button class="remove-button">Remove from cart</button>
-      </div>
+      
       <h3 id="total-price">$ {{ totalPrice }}</h3>
       <button id="checkout-button">Proceed to checkout</button>
     </div>
@@ -42,29 +35,5 @@ const totalPrice = computed(() => {
 
   #checkout-button {
     width: 100%;
-  }
-
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
-
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
-
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
-
-  .remove-button {
-    flex: 1;
-    margin: auto;
   }
 </style>
