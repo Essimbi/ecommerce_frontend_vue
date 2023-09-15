3a0a6873f4eea565a0dcf21defcec5126770e341
Essimbi <essimbideranot@gmail.com>
2023-09-15 07:30:53 +0100

    Découpage du composant ProductsPage en composants ProductsGrid et ProductsGridItem

---
+++ src/components/ProductsGrid.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+import ProductsGridItem from './ProductsGridItem.vue';
+
+const props = defineProps(['products'])
+
+</script>
+
+<template>
+    <div class="grid-wrap">
+        <ProductsGridItem v-for="(product, index) in props.products" :key="index" :product="product" />
+    </div>
+</template>
+
+<style scoped>
+  .grid-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
+</style>
